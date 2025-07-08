ee1fee3317880509f515a09cf5eadc6ac4529125
gwenie-hub <lezzahgwenn@gmail.com>
2025-07-08 16:22:24 +0800

    initial commit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,8 +51,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-
-        // ✅ Spatie Laravel Permission middleware
+    
+        // ✅ Correct Spatie Middleware Paths
         'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
         'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
