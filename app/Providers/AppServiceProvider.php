64a84217ef30b367a37d817f2b4b9de7b8efb0ac
gwenie-hub <lezzahgwenn@gmail.com>
2025-07-08 12:25:45 +0800

    initial commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,23 +4,17 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\Router;
-use Spatie\Permission\Middlewares\RoleMiddleware;
-use Spatie\Permission\Middlewares\PermissionMiddleware;
-use Spatie\Permission\Middlewares\RoleOrPermissionMiddleware;
+use Spatie\Permission\Http\Middleware\RoleMiddleware;
+use Spatie\Permission\Http\Middleware\PermissionMiddleware;
+use Spatie\Permission\Http\Middleware\RoleOrPermissionMiddleware;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register()
     {
-        // No custom login response registered
+        //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         $router = app(Router::class);
